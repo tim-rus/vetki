b313a46f9bcea68e53db1f87bca42a0b4ebdf63d
Tim-Rus <timmymulla@yandex.ru>
2025-10-26 15:32:24 +0500

    feat: window controls

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,20 @@
+import { Application, Window } from "@wailsio/runtime"
+import { createSignal, onMount } from "solid-js"
 import Icon from "../ui/Icon"
 import { SideNav } from "./SideNav"
 import { Tabs } from "./Tabs"
 
 export default function Layout(props) {
+	
+	// max/restore button
+	const [icon, setIcon] = createSignal("square")
+	const makeIcon = async () => setIcon(await Window.IsMaximised() ? "copy" : "square")
+	onMount(()=>{
+		makeIcon()
+		window.addEventListener('resize', makeIcon)
+	})
+
+	//
 	return (
 		<div class="layout">
 			<SideNav />
@@ -11,9 +23,9 @@ export default function Layout(props) {
 					<Tabs />
 					<div class="title-rest"></div>
 					<div class="window-controls">
-						<button><Icon name="minus" /></button>
-						<button><Icon name="copy" /></button>
-						<button><Icon name="x" /></button>
+						<button onclick={Window.Minimise}><Icon name="minus" /></button>
+						<button onclick={Window.ToggleMaximise} class="flip"><Icon name={icon()} /></button>
+						<button onclick={Application.Quit} class="danger"><Icon name="x" /></button>
 					</div>
 				</div>
 				<div class="content">
